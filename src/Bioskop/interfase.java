dfecc348e2e7479317b2ca8e70bb8475d8da2a24
fardinjeer <fardinjeer@gmail.com>
2022-01-16 10:01:01 +0700

    first commit upload project to github

---
+++ src/Bioskop/interfase.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bioskop;
+
+/**
+ *
+ * @author LENOVO
+ */
+interface bioskop{
+    void Welcome (String welcomemessage);
+}
